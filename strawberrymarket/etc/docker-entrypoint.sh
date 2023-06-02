58c2de02e5be604c3627dd7cd1cc29c22f95c7f2
heoseongjin <hsj02036@gmail.com>
2023-06-02 15:39:53 +0900

    chore: nginx 적용

--- strawberrymarket/etc/docker-entrypoint.sh
+++ strawberrymarket/etc/docker-entrypoint.sh
@@ -3,5 +3,5 @@
 set -e
 
 flask db upgrade
-
-gunicorn --bind :8080 --workers 2 --threads 8 --access-logfile - 'app:create_app()'
+# gunicorn --bind :8080 --workers 2 --threads 8 --access-logfile - 'app:create_app()'
+gunicorn --bind unix:/tmp/gunicorn.sock --workers 2 --threads 8 --reload --access-logfile - 'app:create_app()'
